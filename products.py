1e9fffa6a1c5da4c6bfb97f02d48c1df49887c02
parksontyc <parkson.tyc.chen@gmail.com>
2021-02-08 00:33:49 +0800

    f

--- products.py
+++ products.py
@@ -1,14 +1,17 @@
+import os #operating system
 
 products = []
-
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品，價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
-
+if os.path.isfile('products.csv'): #這是相對路徑
+	print('有檔')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品, 價格\n' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('沒檔')
 
 while True:
 	name = input('請輸入商品名稱：')
@@ -22,10 +25,8 @@ while True:
 	products.append(p)
 print(products)
 
-print(products[0][0])
-
 for i in products:
-	print(i)
+	print(i[0], '的價格是', i[1])
 
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品, 價格\n')
